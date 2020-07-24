65efbdf2aa4da8a94d319a0cfd19b5502674d7d6
Daryoush <daryoush_farsimadan@hotmail.com>
2020-07-24 17:45:02 +0300

    init boilerplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import { Component } from "react";
+import React from "react";
+
+function App() {
+  return <div>Hello World</div>;
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react";
+import App from "./src/app";
+
+ReactDOM.render(<App></App>, document.getElementById("root"));
